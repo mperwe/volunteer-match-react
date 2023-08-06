4bd2120ae768371287eef62b0ce853baac53f37e
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-08-06 12:04:04 +0200

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from "react-router-dom";
+import Home from "./components/Home"
+import Services from "./components/Services"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Register from "./components/Register"
+import NotFound from "./components/NotFound"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/services" element={<Services />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
