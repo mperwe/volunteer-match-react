7766e34198b06aca60889442a22c7ed8733bcd75
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-08-07 18:43:33 +0200

    Edit home page setup

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,47 +4,47 @@ import "./contact.css"
 const Contact = () => {
     return (
         <section id="contact" className="contact_us">
-            <div class="contain">
-                <div class="wrapper">
-                    <div class="form">
+            <div className="contain">
+                <div className="wrapper">
+                    <div className="form">
                         <h4>GET IN TOUCH</h4>
-                        <h2 class="form-headline">Send us a message</h2>
+                        <h2 className="form-headline">Send us a message</h2>
                         <form id="submit-form" action="">
                             <p>
-                                <input id="name" class="form-input" type="text" placeholder="Your Name*" />
-                                <small class="name-error"></small>
+                                <input id="name" className="form-input" type="text" placeholder="Your Name*" />
+                                <small className="name-error"></small>
                             </p>
                             <p>
-                                <input id="email" class="form-input" type="email" placeholder="Your Email*" />
-                                <small class="name-error"></small>
+                                <input id="email" className="form-input" type="email" placeholder="Your Email*" />
+                                <small className="name-error"></small>
                             </p>
-                            <p class="full-width">
-                                <input id="company-name" class="form-input" type="text" placeholder="Company Name*" required />
+                            <p className="full-width">
+                                <input id="company-name" className="form-input" type="text" placeholder="Company Name*" required />
                                 <small></small>
                             </p>
-                            <p class="full-width">
+                            <p className="full-width">
                                 <textarea minlength="20" id="message" cols="30" rows="7" placeholder="Your Message*" required></textarea>
                                 <small></small>
                             </p>
-                            <p class="full-width">
+                            <p className="full-width">
                                 <input type="checkbox" id="checkbox" name="checkbox" checked /> Yes, I would like to receive communications by call / email about the application services.
                             </p>
-                            <p class="full-width">
-                                <input type="submit" class="submit-btn" value="Submit" />
-                                <button class="reset-btn" onclick="reset()">Reset</button>
+                            <p className="full-width">
+                                <input type="submit" className="submit-btn" value="Submit" />
+                                <button className="reset-btn" onclick="reset()">Reset</button>
                             </p>
                         </form>
                     </div>
 
-                    <div class="contacts contact-wrapper">
+                    <div className="contacts contact-wrapper">
 
                         <ul>
-                            <li>We've driven online revenues of over <span class="highlight-text-grey">$2
+                            <li>We've driven online revenues of over <span className="highlight-text-grey">$2
                                 billion</span> for our clients. Ready to know
                                 how we can help you?</li>
-                            <span class="hightlight-contact-info">
-                                <li class="email-info"><i class="fa fa-envelope" aria-hidden="true"></i> info@demo.com</li>
-                                <li><i class="fa fa-phone" aria-hidden="true"></i> <span class="highlight-text">+91 11 1111 2900</span></li>
+                            <span className="hightlight-contact-info">
+                                <li className="email-info"><i className="fa fa-envelope" aria-hidden="true"></i> info@demo.com</li>
+                                <li><i className="fa fa-phone" aria-hidden="true"></i> <span className="highlight-text">+91 11 1111 2900</span></li>
                             </span>
                         </ul>
                     </div>
