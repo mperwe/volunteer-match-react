d7a3df1f8a24eb82988a42d570b21e80ae091d97
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-08-13 17:58:35 +0200

    Make UI improvements

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -7,54 +7,50 @@ const Register = () => {
     return (
         <>
             <NavBar />
-            <main>
-                <section className="section-login">
-                    <div className="section-main">
-                        <div className="section-login-1">
-                            <div className="section-login-1-main">
-                                <h1 className="section-login-1-title">Voluteer Link</h1>
-                                <p className="section-login-1-text">Life-changing moments start here.</p>
-                                <div className="section-login-1-img">
-                                    <img src="signup2.jpg" alt="" />
-                                </div>
-
-                            </div>
-                        </div>
-                        <div className="section-login-2">
-                            <div className="section-login-2-main">
-
-                                <h1 className="section-login-2-title">Sign Up</h1>
-                                <form className="section-login-2-form">
-                                    <div className="login-form-1">
-                                        <label for="input-email">Email</label>
-                                        <input type="text" id="input-email" placeholder="org@example.com" required />
-                                    </div>
-                                    <div className="login-form-2">
-                                        <label for="input-name">Organisation Name</label>
-                                        <input type="text" id="input-name" placeholder="Company Name" required />
-                                    </div>
-                                    <div className="login-form-3">
-                                        <label for="input-password">Password</label>
-                                        <input type="password" id="input-password" placeholder="At least 8 characters" required />
-                                    </div>
-                                    <div className="login-form-4">
-                                        <input type="checkbox" id="input-checkbox" />
-                                        <p>By creating an account, you agree to the <a href="#">Terms & Conditions.</a></p>
-                                    </div>
-                                    <div className="login-form-submit-btn">
-                                        <button>Create an Account</button>
-                                    </div>
-                                    <div className="login-form-5">
-                                        <p>Already have an account? <a href="#">Sign In</a></p>
-                                    </div>
-                                </form>
-
+            <section className="section-login">
+                <div className="section-main">
+                    <div className="section-login-1">
+                        <div className="section-login-1-main">
+                            <h1 className="section-login-1-title">Voluteer Link</h1>
+                            <p className="section-login-1-text">Life-changing moments start here.</p>
+                            <div className="section-login-1-img">
+                                <img src="signup2.jpg" alt="signup" />
                             </div>
                         </div>
                     </div>
-                </section>
-            </main>
-            <Footer/>
+                    <div className="section-login-2">
+                        <div className="section-login-2-main">
+                            <h1 className="section-login-2-title">Sign Up</h1>
+                            <form className="section-login-2-form">
+                                <div className="login-form-1">
+                                    <label for="input-email">Email</label>
+                                    <input type="text" id="input-email" placeholder="org@example.com" required />
+                                </div>
+                                <div className="login-form-2">
+                                    <label for="input-name">Organisation Name</label>
+                                    <input type="text" id="input-name" placeholder="Company Name" required />
+                                </div>
+                                <div className="login-form-3">
+                                    <label for="input-password">Password</label>
+                                    <input type="password" id="input-password" placeholder="At least 8 characters" required />
+                                </div>
+                                <div className="login-form-4">
+                                    <input type="checkbox" id="input-checkbox" />
+                                    <p>By creating an account, you agree to the <a href="/terms-and-conditions">Terms & Conditions.</a></p>
+                                </div>
+                                <div className="login-form-submit-btn">
+                                    <button>Create an Account</button>
+                                </div>
+                                <div className="login-form-5">
+                                    <p>Already have an account? <a href="/signin">Sign In</a></p>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </>
 
 
--- src/components/Service/index.jsx
+++ src/components/Service/index.jsx
@@ -3,18 +3,18 @@ import "./services.css"
 
 const Service = () => {
   return (
-    <section id="services" className="section-gallery">
+    <section id="services" className="service-gallery">
       <div className="services-header">
         <h2>Our Services</h2>
-      <p>Here is some information about the services we provide...</p>
+        Here is some information about the services we provide...
       </div>
       <div className="gallery">
-        <img src="services5.jpg" alt="" className="big" />
-        <img src="services2.jpg" alt="" className="big" />
-        <img src="services3.jpg" alt="" className="big" />
-        <img src="services4.jpg" alt="" className="big" />
-        <img src="services7.jpg" alt="" className="big" />
-        <img src="services8.jpg" alt="" className="big" />
+        <img src="services5.jpg" alt="health" className="big" />
+        <img src="services2.jpg" alt="home" className="big" />
+        <img src="services3.jpg" alt="home" className="big" />
+        <img src="services4.jpg" alt="school" className="big" />
+        <img src="services7.jpg" alt="care" className="big" />
+        <img src="services8.jpg" alt="farm" className="big" />
       </div>
     </section>
 
