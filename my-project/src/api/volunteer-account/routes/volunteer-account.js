7fbb45c8da8bfdd8f3271a05e5a7da5441f9ac58
NABAASA RACHAEL <122932238+NabaasaRachael@users.noreply.github.com>
2023-08-24 08:54:33 +0300

    sign up redirects to the dashboard

---
+++ my-project/src/api/volunteer-account/routes/volunteer-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * volunteer-account router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::volunteer-account.volunteer-account');
---
+++ my-project/src/api/volunteer-account/services/volunteer-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * volunteer-account service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::volunteer-account.volunteer-account');
